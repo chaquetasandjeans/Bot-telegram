2d0602e2a61893e2bf667a78a86d403f3579c3c9
chaquetasandjeans <chaquetasandjeans@gmail.com>
2026-09-15 20:47:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+import threading
+from flask import Flask
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+web_app = Flask(__name__)
+@web_app.route('/')
+def home():
+    return "Bot chaquetas activo"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    web_app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_web, daemon=True).start()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("¡Hola! Bot prendido 24/7 en Render")
+    await update.message.reply_text("Hola! Bienvenido a Chaquetas y Vaqueros 🧥 Dime que talla buscas?")
 
-app = Application.builder().token(TOKEN).build()
+app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+print("Bot iniciado...")
 app.run_polling()
